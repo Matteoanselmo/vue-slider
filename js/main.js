a08df3e5aa4ca6241abb7cd4cdf9a133ccc649d4
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-17 14:47:57 +0100

    inizializzazione Vue slider

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+    el: '#app',
+    data : {
+        myArray : [
+            {
+                image : 'img/img1.jpg',
+                titolo : 'Random',
+                paragrafo : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat id quas cupiditate possimus eos ullam beatae nemo reprehenderit? Iusto blanditiis laboriosam nulla'
+            },
+            {
+                image : 'img/img2.jpg',
+                titolo : 'Randomissimo',
+                paragrafo : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat id quas cupiditate possimus eos ullam beatae nemo reprehenderit? Iusto blanditiis laboriosam nulla'
+            },
+            {
+                image : 'img/img3.jpg',
+                titolo : 'Grazie',
+                paragrafo : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat id quas cupiditate possimus eos ullam beatae nemo reprehenderit? Iusto blanditiis laboriosam nulla'
+            },
+            {
+                image : 'img/img4.jpg',
+                titolo : 'Graziella',
+                paragrafo : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat id quas cupiditate possimus eos ullam beatae nemo reprehenderit? Iusto blanditiis laboriosam nulla'
+            },
+            
+            ],
+    },
+});
